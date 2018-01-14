54d4f13718eea38874618b9c8ff45ba2a35eeaad
Toby Sullivan <tobyjsullivan@gmail.com>
2018-01-14 21:06:06 +1100

    Add some test data

--- web/src/reducer/entities/people.js
+++ web/src/reducer/entities/people.js
@@ -1,8 +1,16 @@
 import { Record, Map, List } from 'immutable';
 
+const Conversation = Record({
+  date: '',
+  summary: ''
+})
+
 const Person = Record({
   id: '',
-  name: ''
+  name: '',
+  facts: List([]),
+  convos: List([]),
+  notes: List([])
 })
 
 const State = Record({
@@ -10,15 +18,35 @@ const State = Record({
     byId: Map({
       janetjackson: new Person({
         id: 'janetjackson', 
-        name: 'Janet Jackson'
+        name: 'Janet Jackson',
+        facts: List(['Previous Partner: Wissam Mana. Split 2017-09.', 'Singer/Songwriter', 'Birthday: May 16']),
+        notes: List(['Brother Michael died in 2009.'])
       }),
       michealJ: new Person({
         id: 'michealJ', 
-        name: 'Michael Jackson'
+        name: 'Michael Jackson',
+        facts: List(['Died June 29, 2009.', 'Famous Singer']),
+        notes: List(['Had a cool house with a theme park called Neverland.', 'Sister, Janet, is also a famous artist.'])
       }),
       'toby.s': new Person({
         id: 'toby.s', 
-        name: 'Toby Sullivan'
+        name: 'Toby Sullivan',
+        facts: List(['Partner: Jess', 'Lives in Sydney', 'Works in software']),
+        convos: List([
+          new Conversation({
+            date: '2018-01-01',
+            summary: 'Exchanged happy new year texts'
+          }),
+          new Conversation({
+            date: '2017-12-16',
+            summary: 'Attended his farewell party before he went to Aus. He\'s starting a new job in Feb.'
+          }),
+          new Conversation({
+            date: '2017-11-14',
+            summary: 'Wished him happy birthday on Facebook but he snubbed me.'
+          })
+        ]),
+        notes: List(['Instagram and twitter: @tobyjsullivan'])
       })
     })
   }
