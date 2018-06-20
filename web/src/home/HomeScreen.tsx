6bd4b2a55d14a83eb4d277af276ba639b1a7367b
Toby Sullivan <tobyjsullivan@gmail.com>
2018-06-20 22:02:27 +1000

    Get storybook working with typescript

---
+++ web/src/home/HomeScreen.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default () => (
+  <h1>Welcome Home</h1>
+);
---
+++ web/src/main/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import HomeScreen from '../home/HomeScreen';
+
+const App = () => {
+  return (
+    <div>
+      <HomeScreen />
+      It's good to see you, old friend!
+    </div>
+  );
+};
+
+export default App;
