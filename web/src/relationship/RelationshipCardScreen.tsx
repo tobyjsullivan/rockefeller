48ff8e3582f6c997348ce32321c883370a04062b
Toby Sullivan <tobyjsullivan@gmail.com>
2018-07-14 09:54:36 +1000

    Add home link

--- web/src/relationship/RelationshipCardScreen.tsx
+++ web/src/relationship/RelationshipCardScreen.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import RelationshipCard from '../ui/RelationshipCard';
+import { Group, Link } from '../ui/Els';
+import HomeLink from '../ui/HomeLink';
 
-
-const RelationshipCardScreen = () => (
-  <RelationshipCard name="Ingrid A. Davis" tagline="Worked together at Megacorp" notes="Notes..." />
+const RelationshipCardScreen: React.StatelessComponent = () => (
+  <Group>
+    <HomeLink />
+    <RelationshipCard name="Ingrid A. Davis" tagline="Worked together at Megacorp" notes="Notes..." />
+  </Group>
 );
 
 export default RelationshipCardScreen;
---
+++ web/src/ui/HomeLink.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import {Link } from './Els';
+
+const HomeLink: React.StatelessComponent = () => (<Link to="/">Home</Link>);
+
+export default HomeLink;
