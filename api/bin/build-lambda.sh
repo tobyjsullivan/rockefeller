5627acfaab05412061ee8e5be6a3e706bcf1aeba
Toby Sullivan <tobyjsullivan@gmail.com>
2018-07-29 20:56:31 +1000

    Move API code to src dir

--- api/bin/build-lambda.sh
+++ api/bin/build-lambda.sh
@@ -3,9 +3,9 @@ set -eu
 set -o pipefail
 
 main() {
-    go get -v -d
+    go get -v -d ./src
 
-    GOOS=linux GOARCH=amd64 go build -o build/lambda_handler lambda_handler.go
+    GOOS=linux GOARCH=amd64 go build -o build/lambda_handler src/lambda_handler.go
 
     zip -j build/package.zip build/lambda_handler
 }
--- api/bin/build-local.sh
+++ api/bin/build-local.sh
@@ -3,9 +3,9 @@ set -eu
 set -o pipefail
 
 main() {
-    go get -v -d
+    go get -v -d ./src
 
-    GOOS=darwin GOARCH=amd64 go build -o build/api http/app.go
+    GOOS=darwin GOARCH=amd64 go build -o build/api src/http/app.go
 }
 
 main "$@"
